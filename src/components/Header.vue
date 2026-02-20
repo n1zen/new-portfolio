ea935748277f85020abe17e55d490a2389a40e49
n1zen <thisisnez1.2021@gmail.com>
2026-02-20 16:44:14 +0800

    feat: scaffold basic Vue application with global CSS, header component, and initial app setup.

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <a href="#home">
+                    <span class="logo-typing"></span>
+                    <span class="logo-cursor">|</span>
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#achievements">Achievements</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li>
+                        <button id="theme-toggle">
+                            <i class="fa-solid fa-sun"></i>
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
